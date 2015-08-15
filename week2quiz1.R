994fde147a7ab8e67e40dcbbc5c55ce0a0b9cd67
yewchoong <yewchoong@hotmail.com>
2015-08-15 15:35:21 +0800

    Update week2quiz1.R

--- week2quiz1.R
+++ week2quiz1.R
@@ -1,3 +1,6 @@
+# Question 1
+#Register an application with the Github API here https://github.com/settings/applications. Access the API to get information on your instructors repositories (hint: this is the url you want "https://api.github.com/users/jtleek/repos"). Use this data to find the time that the datasharing repo was created. What time was it created? This tutorial may be useful (https://github.com/hadley/httr/blob/master/demo/oauth2-github.r). You may also need to run the code in the base R package and not R studio. 
+
 library(httr)
 
 # 1. Find OAuth settings for github:
